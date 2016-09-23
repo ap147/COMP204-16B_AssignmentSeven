ce92c2459a85c6c4098953dc5e47661f78f1b037
Parmar, Amarjot <amarjot-parmar@hotmail.co.nz>
2016-09-24 02:33:58 +1200

    Implemented DrawableView, Red Cube gets smaller & smaller & disapears.

--- BallApp/app/src/main/java/com/amarjot8/ballapp/MainActivity.java
+++ BallApp/app/src/main/java/com/amarjot8/ballapp/MainActivity.java
@@ -1,13 +1,36 @@
 package com.amarjot8.ballapp;
 
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
+    int rx, ry = 0;
+
+    public class DrawingView extends View{
+        public DrawingView(Context c){
+            super(c);
+        }
+
+        @Override
+        protected void onDraw(Canvas c)
+        {
+            Paint p = new Paint();
+            p.setColor(Color.RED);
+            c.drawRect(rx, ry, 100, 100, p);
+            rx++; ry++;
+            invalidate();
+        }
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        DrawingView dv = new DrawingView(this);
+        setContentView(dv);//R.layout.activity_main);
     }
 }

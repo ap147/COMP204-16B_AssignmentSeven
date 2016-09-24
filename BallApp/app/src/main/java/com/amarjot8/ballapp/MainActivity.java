efbb28d134cbcc0241a9d90df5f85bc786080f2c
Parmar, Amarjot <amarjot-parmar@hotmail.co.nz>
2016-09-25 04:10:06 +1300

    Documentation DONE, PROJECT COMPLETED

--- BallApp/app/src/main/java/com/amarjot8/ballapp/MainActivity.java
+++ BallApp/app/src/main/java/com/amarjot8/ballapp/MainActivity.java
@@ -27,17 +27,20 @@ import java.util.List;
 //http://stackoverflow.com/questions/26543268/android-making-an-app-fullscreen
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
-    DrawingView dv;
-
+    //Circle Size
+    int Circle_radius = 50;
     //Circle Cords
     int Circle_x = 50; int Circle_y = 50;
+    //Speed Ball travel at
+    int speed_x, speed_y = 0;
 
     //sensor's values will be sored
     float sensor_x = 0, sensor_y = 0, sensor_z = 0;
-    //Speed
-    int speed_x, speed_y = 0;
+
+    //The amount of time a circle is drawed & gets displayed on screen top left
     int counter = 0;
 
+    //Used to decide how to draw ball
     boolean FingerDownOnBall = false;
 
     //Used to store Sesnor/ Register listner / Unregister listner
@@ -54,8 +57,6 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
             sensor_x = event.values[0];
             sensor_y = event.values[1];
             sensor_z = event.values[2];
-
-        //    System.out.println("sx : " + sensor_x + " sy : " + sensor_y + " sz : " + sensor_z );
         }
     }
 
@@ -87,17 +88,12 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
             }
         }
 
-        //Circle Size
-        int Circle_radius = 50;
-
-      //  public PointerPoint primary = null;
-       // List<PointerPoint> pointers = new ArrayList<>();
 
         public DrawingView(Context c)
         {
             super(c);
         }
-
+        //Evertime the screen is touched
         @Override
         public boolean onTouchEvent(MotionEvent event)
         {
@@ -106,19 +102,14 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
             {
                 //User Puts finger down
                 case MotionEvent.ACTION_DOWN:
-                    if(IsFingerOnBall((int) event.getX(),(int) event.getY()))
-                    {
-                        Circle_x = (int) event.getX();
-                        Circle_y = (int) event.getY();
-                    }
-                   //
-                        // pointers.add(new PointerPoint((int) event.getX(), (int) event.getY(), 0));
+                    //Check if finger is on ball
+                    IsFingerOnBall((int) event.getX(),(int) event.getY());
                     break;
                 //Moving finger across screen
                 case MotionEvent.ACTION_MOVE:
+                    //While the finger is on Ball Keep updating Balls cordinates
                     if(FingerDownOnBall)
                     {
-                     //   primary = null;
                         Circle_x = (int) event.getX();
                         Circle_y = (int) event.getY();
                     }
@@ -127,81 +118,71 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
                 case MotionEvent.ACTION_UP:
                     FingerDownOnBall = false;
                     break;
-                case MotionEvent.ACTION_POINTER_UP:
-                    break;
-                case  MotionEvent.ACTION_POINTER_DOWN:
-                    break;
             }
-
             return true;
         }
 
         @Override
         protected void onDraw(Canvas c)
         {
-             DrawCircleAndText(c, Circle_x, Circle_y, Circle_radius);
+            //Draws the ball
+            DrawCircleAndText(c, Circle_x, Circle_y, Circle_radius);
+            //If ball isnt picked up then let gravity sensor act on it
             if(!FingerDownOnBall)
             {
-             speed_x -= sensor_x;
-             sensor_y += sensor_y;
+                speed_x -= sensor_x;
+                speed_y += sensor_y;
 
-             speed_x *= 0.9f;
-             sensor_y *= 0.9f;
+                Circle_x += speed_x;
+                Circle_y += speed_y;
 
-              Circle_x += speed_x;
-             Circle_y += sensor_y;
-
-               if (Circle_x - Circle_radius < 0) { Circle_x = 0 + Circle_radius; speed_x = 0; }
-               if (Circle_x + Circle_radius > c.getWidth()) { Circle_x = c.getWidth() - Circle_radius; speed_x = 0; }
-               if (Circle_y  - Circle_radius< 0) { Circle_y = 0 + Circle_radius; sensor_y = 0; }
+                //When ball is going outside the phone, it stops it.
+                if (Circle_x - Circle_radius < 0) { Circle_x = 0 + Circle_radius; speed_x = 0; }
+                if (Circle_x + Circle_radius > c.getWidth()) { Circle_x = c.getWidth() - Circle_radius; speed_x = 0; }
+                if (Circle_y  - Circle_radius< 0) { Circle_y = 0 + Circle_radius; speed_y = 0; }
                 if (Circle_y + Circle_radius > c.getHeight()) { Circle_y = c.getHeight() - Circle_radius; speed_y = 0; }
             }
-
+            //Redraw
             invalidate();
         }
 
     }
-
+    //Draws ball Green, if ball finger is on ball then draws it in Red and increases size
     protected void DrawCircleAndText(Canvas c,int x, int y , int radius)
     {
-
         Paint p = new Paint();
         p.setColor(Color.GREEN);
-
+        //Draw Reg Big ball
         if(FingerDownOnBall)
         {
             p.setColor(Color.RED);
             c.drawCircle(x,y,75, p);
         }
+        //Draw Default ball
         else
         {
             c.drawCircle(x,y,radius, p);
         }
-
-
-
+        //Drawing counter
         p.setColor(Color.BLUE);
         p.setTextSize(55);
         c.drawText("Counter :"+counter,10,100, p);
         counter++;
     }
 
+    //uses correct offsets and checks if cordinates given are touching the ball
     protected boolean IsFingerOnBall(int EventX, int EventY)
     {
-        boolean result = false;
-
+        //If finger is on ball
         if((EventX <= (Circle_x + 50) && (EventX > Circle_x - 50)) && (EventY <= (Circle_y + 50) && (EventY > Circle_y - 50)))
         {
-            result =true;
             FingerDownOnBall = true;
+            return true;
         }
         else{
             FingerDownOnBall = false;
+            return false;
         }
-
-        System.out.println(result);
-        //System.out.println("Ball X" + BallX + " Ball Y " +  BallY + " Event X " + EventX + " EventY " + EventY + "  RESULT " + result);
-        return result;
     }
 
     @Override
